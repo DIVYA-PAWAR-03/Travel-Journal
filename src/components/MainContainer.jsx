fdad6e0305a47a8f62c300c79ac3c295cbe1070a
DIVYA-PAWAR-03 <121420835+DIVYA-PAWAR-03@users.noreply.github.com>
2025-01-06 19:35:38 +0530

    used props

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,25 +1,25 @@
 
 
-export default function MainContainer(){
+export default function MainContainer(props){
     return(
         <main>
             
                 <div className="container1">
                 <div >
-                    <img src="/images/mountain.jpg" alt="Mountain" className="image1" />
+                    <img src={props.img.src} alt={props.img.alt}className="image1" />
                 </div>
                 <div className="desc">
-                    <h2>Switzerland</h2>
-                    <a href="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=swizrland&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">View on Google Maps</a>
-                    <h3>27 May,2024 - 8 June,2024</h3>
-                    <p className="para1">Switzerland, a haven for nature lovers and adventure seekers, is renowned for its stunning Alpine landscapes, crystal-clear lakes, and charming cities. Iconic spots include the Matterhorn in Zermatt, Jungfraujoch's "Top of Europe," and the picturesque towns of Interlaken and Lucerne. Visitors can explore medieval castles like Château de Chillon, enjoy the grandeur of Rhine Falls, or immerse themselves in the cultural vibes of Zurich and Geneva. With its efficient transport, delicious Swiss chocolate, and warm hospitality, Switzerland is a dream destination for travelers worldwide.</p>
+                    <h2>{props.city}</h2>
+                    <a href={props.location}>View on Google Maps</a>
+                    <h3>{props.dates}</h3>
+                    <p className="para1">{props.description}</p>
                 </div>
                 </div>
 
 
-                <div className="container2">
+                {/* <div className="container2">
                 <div >
-                    <img src="/images/bridge.jpg" alt="Mountain" className="image" />
+                    <img src="/images/bridge.jpg" alt="bridge" className="image" />
                 </div>
                 <div className="desc">
                     <h2>Norway</h2>
@@ -27,12 +27,12 @@ export default function MainContainer(){
                     <h3>31 July,2024 - 5 Aug,2024</h3>
                     <p className="para1">Norway, known for its dramatic fjords, northern lights, and rich Viking history, is a paradise for nature and adventure enthusiasts. Iconic destinations include the Geirangerfjord, the Lofoten Islands, and the Arctic town of Tromsø, perfect for viewing the aurora borealis. Visitors can explore Oslo’s vibrant culture, hike the Pulpit Rock, or take scenic rail journeys like the Flåm Railway. With its midnight sun in summer and cozy winter charm, Norway offers unforgettable experiences year-round.</p>
                 </div>
-                </div>
+                </div> */}
 
 
-                <div className="container3">
+                {/* <div className="container3">
                 <div >
-                    <img src="/images/river.jpg" alt="Mountain" className="image" />
+                    <img src="/images/river.jpg" alt="river" className="image" />
                 </div>
                 <div className="desc">
                     <h2>Japan</h2>
@@ -40,7 +40,7 @@ export default function MainContainer(){
                     <h3>26 Nov ,2024 - 7 Dec,2024</h3>
                     <p className="para1">Japan, a land of timeless tradition and cutting-edge modernity, offers a unique blend of cultural and natural attractions. Visitors can marvel at historic landmarks like Kyoto’s temples, the iconic Mount Fuji, and Hiroshima’s Peace Memorial. Tokyo dazzles with its futuristic skyline, while Osaka is famed for its street food. The cherry blossoms in spring and autumn foliage provide breathtaking scenery. From ancient castles to hot springs and vibrant festivals, Japan promises an unforgettable journey for every traveler.</p>
                 </div>
-                </div>
+                </div> */}
 
                 
 
