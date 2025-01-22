67c4c9da9842c386cbac4a9fe8f9815d9e679f32
DIVYA-PAWAR-03 <121420835+DIVYA-PAWAR-03@users.noreply.github.com>
2025-01-22 19:43:32 +0530

    added new information

--- src/components/data.js
+++ src/components/data.js
@@ -35,6 +35,33 @@ export default[
       location:"https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttuhttps://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=japan&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
       dates:"26 Nov ,2024 - 7 Dec,2024",
       description:"Japan, a land of timeless tradition and cutting-edge modernity, offers a unique blend of cultural and natural attractions. Visitors can marvel at historic landmarks like Kyoto’s temples, the iconic Mount Fuji, and Hiroshima’s Peace Memorial. Tokyo dazzles with its futuristic skyline, while Osaka is famed for its street food. The cherry blossoms in spring and autumn foliage provide breathtaking scenery. From ancient castles to hot springs and vibrant festivals, Japan promises an unforgettable journey for every traveler."
-            }
-        
+            },
+
+
+            {
+                id:4,
+                img:{
+                    src:"/images/paris.jpg",
+            alt:"river"
+          },
+          city:"Paris",
+          location:"https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttuhttps://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=japan&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+          dates:"22 Nov ,2024 - 25 nov,2024",
+          description:"Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré."
+                },
+
+
+                {
+                    id:5,
+                    img:{
+                        src:"/images/Thailand.webp",
+                alt:"river"
+              },
+              city:"Thailand",
+              location:"https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttuhttps://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=japan&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+              dates:"8 Dec ,2024 - 10 Dec,2024",
+              description:"Thailand is a Southeast Asian country. It's known for tropical beaches, opulent royal palaces, ancient ruins and ornate temples displaying figures of Buddha. In Bangkok, the capital, an ultramodern cityscape rises next to quiet canalside communities and the iconic temples of Wat Arun, Wat Pho and the Emerald Buddha Temple (Wat Phra Kaew). Nearby beach resorts include bustling Pattaya and fashionable Hua Hin"
+                    }
+
+                    
 ]
